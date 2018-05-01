3707e77d3911d619ea896a65eb5ff579daac89a8
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-04-30 20:12:34 -0500

    Feat - Update. Completing the initial get request for the server with proper comments

--- mock.py
+++ mock.py
@@ -1,2 +1,31 @@
-def __init__(self, config):
-    signal.signal
+from http.server import BaseHTTPRequestHandler
+from http.server import HTTPServer
+from urllib.parse import urlparse
+
+
+class myHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        ## sending response header
+        self.send_response(200)
+        self.send_header('content-type','text/html')
+        self.end_headers()
+
+        response = "Hello World. Am running"
+
+        self.wfile.write(bytes(response, "utf-8"))
+        ## python always returns values
+        return
+
+def run():
+  print('starting server...')
+
+  # Server settings
+  # running the server on port 8081
+  server_address = ('127.0.0.1', 8081)
+  httpd = HTTPServer(server_address, myHandler)
+  print('running server...')
+  httpd.serve_forever()
+
+
+run()
